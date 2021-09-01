d65d88af9a1c9f89ab790bbd7b3ca124276ea74e
nathbout2 <nathbout2@gmail.com>
2021-09-01 11:05:34 +0200

    modif boris

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
+function register_assets() {
 
 // Lier le style.css
 wp_enqueue_style( 
-    'style.css',
+    'wordpress-style',
     get_stylesheet_uri(), 
     array(), 
     '1.0'
@@ -10,5 +11,10 @@ wp_enqueue_style(
 
 // Lier le JS
 wp_enqueue_script( 
-    'js/script.js',
+    'custom-script',
+    get_template_directory_uri() . '/js/script.js',
+    array(), 
+    '1.0'
 );
+}
+add_action( 'wp_enqueue_scripts', 'register_assets' );
