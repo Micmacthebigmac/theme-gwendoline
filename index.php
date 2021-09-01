8adaa3142ec0c98640c9bc4be85b11d87308fda5
“laury <lquarto.lq@gmail.com>
2021-09-01 11:37:42 +0200

    boucle wp pour le contenu

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
-<h1>Coucou Nathan et Hervé</h1>
-{{[custom-facebook-feed feed=1]}}
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+        
+	<?php the_content(); ?>
+
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
