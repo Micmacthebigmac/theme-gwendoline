ca3334ccccc0b4af8e035fd21cd0b3a6709e6649
Hervé <h.richard.codeur.onlinem>
2021-09-01 10:33:57 +0200

    site gwendo header bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php get_header(); ?>
 
-<h1>Coucou</h1>
+
+
+<div class="fond-rouge">
+    <h1 class="texte-blanc"><?php bloginfo('name'); ?></h1> 
+</div>
+
+<div class="fond-jaune">
+
+<?php get_header(); ?>
+
+	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    	<h1><?php the_title(); ?></h1>
+    
+    	<?php the_content(); ?>
+
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
+
+</div>
+
+<div class="fond-rouge">
+    <h1 class="texte-blanc"><?php bloginfo('description'); ?></h1> 
+</div>
 
 <?php get_footer(); ?>
